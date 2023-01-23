db938d8ef968baeef8f2668c6c3a3bd4f1daffb2
Cranky Bawler <6666lenia6666@mail.ru>
2023-01-23 20:46:49 +0300

    Домашнее задание Переменные

--- src/Main.java
+++ src/Main.java
@@ -33,13 +33,13 @@ public class Main {
     }
     public static void task3 () {
         System.out.println("Задача 3");
-        var dog = 8.0;
+        var dog = 12.0;
         dog = dog - 3.5;
         System.out.println(dog);
-        var cat = 3.6;
+        var cat = 7.6;
         cat = cat - 1.6;
         System.out.println(cat);
-        var paper = 763789;
+        var paper = 763793;
         paper = paper - 7639;
         System.out.println(paper);
     }
